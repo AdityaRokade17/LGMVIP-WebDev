e9d19fe8488ce483fd2588db2eb1eae6c88a07b3
Aditya Rokade <89978003+AdityaRokade17@users.noreply.github.com>
2021-10-24 13:44:35 +0530

    Task 2
    Only Important Files Pasted Because Of Size Isuue

---
+++ Task 2-React loader web app/src/Extra Assests/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div class="contain">
+          <div class="col">
+            <h1>ABLEFLOOR</h1>
+            <ul>
+              <li>
+                Email: <a href="mailto:xyz@gmail.com">adityarokade176@gmail.com</a>
+              </li>
+              <li>
+                Phone: <a href="tel: +123456789012">+91988180488</a>
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
